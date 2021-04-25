b5138f01f5dbd0bdfe296d6cfcc6e374bd780b3d
HGattermeyer <henrique1989@gmail.com>
2021-04-25 08:55:40 -0300

    implemented randomCards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Game from "./components/card/game";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Game />
+
+        {/* <Card id={Math.floor(Math.random() * 52) + 1} />
+        <Card id={Math.floor(Math.random() * 52) + 1} />
+        <Card id={Math.floor(Math.random() * 52) + 1} />
+        <Card id={Math.floor(Math.random() * 52) + 1} />
+        <Card id={Math.floor(Math.random() * 52) + 1} /> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/card/card.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Card extends Component {
+  render() {
+    return <div>card {this.props.id}</div>;
+  }
+}
+
+export default Card;
